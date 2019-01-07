4bb58a9caacf0d9a4c84e72d76154bf1620aba01
pylypkutovoi <pylyp.kutov@gmail.com>
2019-01-07 14:35:44 +0100

    add upload block

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
+  var send = document.querySelector(".form__submit");
+  var modalSuccess = document.querySelector(".modal-success")
+  var modalSuccessClose = document.querySelector(".modal-success__close")
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -11,4 +14,14 @@
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
     }
+  });
+
+  send.addEventListener('click', function(event) {
+    event.preventDefault();
+    modalSuccess.classList.add('modal-success--show');
+  });
+
+  modalSuccessClose.addEventListener('click', function(event) {
+    event.preventDefault();
+    modalSuccess.classList.remove('modal-success--show');
   });
